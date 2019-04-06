343f8574883a6a5f5a1167484db3dfeb434e883b
marcik357 <marcik357@gmail.com>
2019-04-06 23:56:10 +0300

    added tabs and author portfolio on product-details page

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$",
+    skin: "round"
+  });
+
+  $('input, select').styler();
+
+  $(".tabs__wrapper .tab").click(function () {
+    $(".tabs__wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tabs__wrapper .tab-item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
+
+});
