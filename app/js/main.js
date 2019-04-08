36a3f79cfb8a98a2169c22a88cd328ba639dae44
marcik357 <marcik357@gmail.com>
2019-04-08 23:15:16 +0300

    added classmates blocks

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,7 @@ $(function(){
 
   $('input, select').styler();
 
+
   $(".tabs__wrapper .tab").click(function () {
     $(".tabs__wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
     $(".tabs__wrapper .tab-item").hide().eq($(this).index()).fadeIn()
